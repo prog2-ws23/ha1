934928ee4a5dd95f0ff88bbeb6dc1de000eab475
Marcel Ratsch <s0589160@htw-berlin.de>
2023-10-16 17:41:32 +0200

    added new test for substraction

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -89,6 +89,22 @@ class CalculatorTest {
     }
 
 
+    @Test
+    @DisplayName("should correctly subtract")
+    void testSubstraction(){
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(4);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "2";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
     //TODO hier weitere Tests erstellen
 }
 
