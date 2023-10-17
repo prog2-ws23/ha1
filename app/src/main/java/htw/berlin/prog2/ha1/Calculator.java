f1cd384e6f4dd8f1e005ff4cd0101441c194c7c3
Tobias Rosenberg <tobias.ros@outlook.de>
2023-10-17 19:30:07 +0200

    Fix für 2. Roten Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -60,6 +60,7 @@ public class Calculator {
      * @param operation "+" für Addition, "-" für Substraktion, "x" für Multiplikation, "/" für Division
      */
     public void pressBinaryOperationKey(String operation)  {
+        if(latestOperation != "") pressEqualsKey();
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
     }
