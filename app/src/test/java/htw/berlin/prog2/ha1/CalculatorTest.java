a219b5d68ab71debbcaf8bb17ef66157a9b51a7c
Suzan Korkmaz <Suzan.Korkmaz@Student.HTW-Berlin.de>
2023-10-22 15:59:45 +0200

    Teil 1 neuer grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -87,6 +87,23 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    @DisplayName("should display result after subtracting two positive multi-digit numbers")
+    void testSubtraction(){
+        Calculator calc =new Calculator();
+        calc.pressDigitKey(3);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(5);
+        calc.pressEqualsKey();
+
+        String expected ="15";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+    }
 
 
     //TODO hier weitere Tests erstellen
