d72485b6a5ccb133b7c2cbda8e50bfe96c4a7247
Your Name <you@example.com>
2023-10-22 10:18:03 +0200

    aufgabe 1: grüne Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,28 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+
+
+    /* Aufgabe 1:Schreiben Sie einen neuen zusätzlichen Test, der eine bisher nicht getestete Funktionalität abdeckt,
+     die bereits funktioniert und der daher direkt grün wird.
+      */
+    @Test
+    @DisplayName("should display result after subtracting two positive multi-digit numbers")
+    void testPositiveSubtraction() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+
+        String expected = "10";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
+
 
