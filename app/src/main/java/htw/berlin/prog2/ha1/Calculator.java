43709a63d5c1c20fafc957dc1c31720f0f3a8425
Thien Huong Pham <thienhuong018@gmail.com>
2023-10-26 22:38:37 +0200

    5th commit - change pressDigitKey() method for redtest2

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -31,7 +31,8 @@ public class Calculator {
     public void pressDigitKey(int digit) {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
+        if(screen.equals("0") || (latestValue == Double.parseDouble(screen) && !screen.equals("-0"))) screen = "";
+        if(screen.equals("-0")) screen = "-";
 
         screen = screen + digit;
     }
