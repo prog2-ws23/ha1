8f2a94a2b89ffb0de618af1bc209d3fef7bde943
Jerome Pincus <Jerome.Pincus@Student.HTW-Berlin.de>
2023-10-26 10:30:01 +0200

    1. Commit für neuer grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTestJerome.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTestJerome.java
@@ -6,5 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTestJerome {
 
+    @Test
+        //"should change the sign of a positive number to negative"
+    void testChangeSignPositiveToNegative() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(5);
+        calc.pressNegativeKey();
+
+        String expected = "-5";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 
 }
