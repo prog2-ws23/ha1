80a0e6dbea7a13c3ace382860b845f8e1306a549
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2023-10-26 16:28:10 +0200

    Teilaufgabe 1 updated

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -126,11 +126,10 @@ public class Calculator {
     public void pressEqualsKey() {
 
         var result = switch (latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
+            case "+", "" -> latestValue + Double.parseDouble(screen);
             case "-" -> latestValue - Double.parseDouble(screen);
             case "x" -> latestValue * Double.parseDouble(screen);
             case "/" -> latestValue / Double.parseDouble(screen);
-            case "" -> latestValue + Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
