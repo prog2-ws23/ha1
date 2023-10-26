e245582408d94fdd48682d6ac39e22726f9bd85c
Benedict Heidecker <benedict.heidecker@gmx.de>
2023-10-26 15:34:53 +0200

    4. zweiter Bugfix dritte Aufgabe

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -12,6 +12,8 @@ public class Calculator {
 
     private double latestValue;
 
+    private double secondValue;
+
     private String latestOperation = "";
 
     /**
@@ -119,16 +121,30 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
-        var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
-            default -> throw new IllegalArgumentException();
-        };
+        var result = 0.0;
+        if (secondValue != 0) {
+            result = switch (latestOperation) {
+                case "+" -> Double.parseDouble(screen) + secondValue;
+                case "-" -> Double.parseDouble(screen) - secondValue;
+                case "x" -> Double.parseDouble(screen) * secondValue;
+                case "/" -> Double.parseDouble(screen) / secondValue;
+                default -> throw new IllegalArgumentException();
+            };
+        } else {
+            secondValue = Double.parseDouble(screen);
+            result = switch (latestOperation) {
+                case "+" -> latestValue + Double.parseDouble(screen);
+                case "-" -> latestValue - Double.parseDouble(screen);
+                case "x" -> latestValue * Double.parseDouble(screen);
+                case "/" -> latestValue / Double.parseDouble(screen);
+                default -> throw new IllegalArgumentException();
+
+            };
+        }
+
         screen = Double.toString(result);
-        if(screen.equals("Infinity")) screen = "Error";
-        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if (screen.equals("Infinity")) screen = "Error";
+        if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+        if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
     }
 }
