64f89b4673d8a83d2fe3bfb6c61723fc435c7969
DavidNgo <David.Ngo@Student.HTW-Berlin.de>
2023-10-27 15:43:49 +0200

    Commit 3 Final

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -94,17 +94,14 @@ class CalculatorTest {
     @DisplayName("should display result after minus")
     void testPositiveMinus() {
         Calculator calc = new Calculator();
-
         calc.pressDigitKey(2);
         calc.pressDigitKey(0);
         calc.pressBinaryOperationKey("-");
         calc.pressDigitKey(2);
         calc.pressDigitKey(0);
         calc.pressEqualsKey();
-
         String expected = "0";
         String actual = calc.readScreen();
-
         assertEquals(expected, actual);
     }
 
