1a545481ff04b7530cf48727034187f2189fdaf6
Emre Tekgül <Emre.Tekguel@Student.HTW-Berlin.de>
2023-10-27 15:33:16 +0200

    Teilaufgabe 3 Bugfix 2

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -75,6 +75,7 @@ public class Calculator {
      * Quadratwurzel, Prozent, Inversion, welche nur einen Operanden benötigen.
      * Beim Drücken der Taste wird direkt die Operation auf den aktuellen Zahlenwert angewendet und
      * der Bildschirminhalt mit dem Ergebnis aktualisiert.
+     * Es das Ergebnis wird Modulo 1 genommen um herauszufinden, ob das Ergebnis ein ganze Zahl ist oder nicht
      * @param operation "√" für Quadratwurzel, "%" für Prozent, "1/x" für Inversion
      */
     public void pressUnaryOperationKey(String operation) {
@@ -86,7 +87,12 @@ public class Calculator {
             case "1/x" -> 1 / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
-        screen = Double.toString(result);
+        if (result % 1 == 0) {
+            screen = Integer.toString((int) result);
+        } else {
+            screen = Double.toString(result);
+        }
+
         if(screen.equals("NaN")) screen = "Error";
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
