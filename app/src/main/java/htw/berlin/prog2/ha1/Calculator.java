a55d08f91749c7f289d45cebf2c4a54ef8775d3a
Sajjad221 <Mohammad.Rezai@Student.HTW-Berlin.de>
2023-10-27 14:38:08 +0200

    Aufgabe 2.1 bug fix

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -84,6 +84,8 @@ public class Calculator {
         screen = Double.toString(result);
 
         if (screen.equals("NaN")) screen = "Error";
+        // Teilaufgabe 3
+        if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
         if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
     }
 
