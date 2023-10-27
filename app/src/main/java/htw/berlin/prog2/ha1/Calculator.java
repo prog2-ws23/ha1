39a5e9fd23443fdeebfbf8ea4d1b5c41cc25f64b
ardaa <Arda.Oeztuerk@Student.HTW-Berlin.de>
2023-10-27 14:24:42 +0200

    3. Teilaufgabe (2. roter Test)

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -132,6 +132,10 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
+        if (latestOperation.isEmpty()){
+            return;
+        }
+
         var result = switch(latestOperation) {
             case "+" -> latestValue + Double.parseDouble(screen);
             case "-" -> latestValue - Double.parseDouble(screen);
