d2456940962861b8099fbe7766f7a5b6472354a8
Massimo Marsiglia <marsigliamassi@gmail.com>
2023-10-27 10:44:52 +0200

    Rote tests sind jetzt Grün

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -107,11 +107,14 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("should display result after subtracting two positive multi-digit numbers")
+    @DisplayName("should display result after adding multiple positive multi-digit numbers")
     void testMultiplePositiveAdditions() {
         Calculator calc = new Calculator();
 
         calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(1);
         calc.pressDigitKey(0);
         calc.pressBinaryOperationKey("+");
         calc.pressDigitKey(2);
@@ -121,14 +124,14 @@ class CalculatorTest {
         calc.pressDigitKey(0);
         calc.pressEqualsKey();
 
-        String expected = "60";
+        String expected = "70";
         String actual = calc.readScreen();
 
         assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("should display the input number")
+    @DisplayName("should display the input number when no operators are used")
     void equalsWithoutOperation() {
         Calculator calc = new Calculator();
 
@@ -140,6 +143,5 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
-    //TODO hier weitere Tests erstellen
 }
 
